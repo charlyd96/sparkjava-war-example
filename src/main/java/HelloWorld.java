3e349fe725681cfb9c711c10090e67b9bdf09f85
charlyd96 <carlosdesiderio96@gmail.com>
2021-12-03 03:04:34 -0300

    asd

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/unaRuta", (req, res) -> "Hello World a todos");
+		get("/unaRuta", (req, res) -> "Hello World a todos y a todas todas");
 	}
 }
